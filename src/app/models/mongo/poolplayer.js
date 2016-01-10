f9c6df26e914905e72612651c3568023eb8bfff2
Marco Prins <marco.prins@icloud.com>
2016-01-10 17:03:14 +0100

    - added extra validation

--- src/app/models/mongo/poolplayer.js
+++ src/app/models/mongo/poolplayer.js
@@ -10,12 +10,20 @@
       'birthday' : {
         type: String, // TODO try making this a Date and prevent Casting before validation
         required: 'Je moet een geboortedatum invullen.',
-        validate: {
-          validator: function(value) {
-            return (value.length == 10 && /^\d+$/.test(value.replace(/\//g, '')) && !isNaN(new Date(value).getTime()));
+        validate: [
+          {
+            validator: function(value) {
+              return (value.length == 10 && /^\d+$/.test(value.replace(/\//g, '')) && !isNaN(new Date(value).getTime()));
+            },
+            message: 'Datum is incorrect (dd-mm-jjjj)'
           },
-          message: 'Datum is incorrect (dd-mm-jjjj)'
-        }
+          {
+            validator: function(value) {
+              return Date.now() >= new Date(value).getTime();
+            },
+            message: 'Datum kan niet in de toekomst liggen'
+          }
+        ]
       },
       'notes': String
   });
