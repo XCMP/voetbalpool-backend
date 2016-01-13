65cda0359a1df43531c67d8ead687641e5b6e883
Marco Prins <marco.prins@icloud.com>
2016-01-13 20:26:47 +0100

    - code formatting

--- src/app/models/mongo/poolplayer.js
+++ src/app/models/mongo/poolplayer.js
@@ -3,29 +3,33 @@
   mongoose.connect('mongodb://localhost:27017/voetbalpool');
 
   var PoolplayerSchema = new mongoose.Schema({
-      'name' : {
-        type: String,
-        required: 'Je moet een naam invullen.'
-      },
-      'birthday' : {
-        type: String, // TODO try making this a Date and prevent Casting before validation
-        required: 'Je moet een geboortedatum invullen.',
-        validate: [
-          {
-            validator: function(value) {
-              return (value.length == 10 && /^\d+$/.test(value.replace(/\//g, '')) && !isNaN(new Date(value).getTime()));
-            },
-            message: 'Datum is incorrect (dd-mm-jjjj)'
+
+    'name' : {
+      type: String,
+      required: 'Je moet een naam invullen.'
+    },
+
+    'birthday' : {
+      type: String, // TODO try making this a Date and prevent Casting before validation
+      required: 'Je moet een geboortedatum invullen.',
+      validate: [
+        {
+          validator: function(value) {
+            return (value.length == 10 && /^\d+$/.test(value.replace(/\//g, '')) && !isNaN(new Date(value).getTime()));
           },
-          {
-            validator: function(value) {
-              return Date.now() >= new Date(value).getTime();
-            },
-            message: 'Datum kan niet in de toekomst liggen'
-          }
-        ]
-      },
-      'notes': String
+          message: 'Datum is incorrect (dd-mm-jjjj)'
+        },
+        {
+          validator: function(value) {
+            return Date.now() >= new Date(value).getTime();
+          },
+          message: 'Datum kan niet in de toekomst liggen'
+        }
+      ]
+    },
+
+    'notes': String
+    
   });
 
   module.exports = mongoose.model('Poolplayer',PoolplayerSchema);
